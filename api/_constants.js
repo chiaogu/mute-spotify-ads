37de210d1f21a1ca360f1bc91ed5c3f839e077d9
chiao <chiaogu@gmail.com>
2020-02-24 18:10:26 -0800

    update host url

--- api/_constants.js
+++ api/_constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
   CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-  REDIRECT_URL: 'https://mute-spotify-ads.chiaogu.now.sh/api/login-callback'
+  REDIRECT_URL: 'https://mute-spotify-ads.now.sh/api/login-callback'
 }
